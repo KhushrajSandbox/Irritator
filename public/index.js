7fe41fdddae9f92996303f6bd99645ea3d75c215
Glitch (irritator) <none>
2019-12-03 11:26:00 +0000

    Import of khushrajrathod/irritatorserver

--- public/index.js
+++ public/index.js
@@ -3,3 +3,13 @@
 // document.getElementById("button-cancel").addEventListener("click", function(){
 //     console.log("resetting");
 // }); 
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
